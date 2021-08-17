c424c9146feb2f09ef887407af3811fd051807e0
moroz-art-dev <moroz.art.dev@gmail.com>
2021-08-17 23:18:46 +0300

    add of lessons in blocks 5

---
+++ learning-reactjs-course-1/block_5/lesson_5_5/Car/Car.js
@@ -0,0 +1,63 @@
+import React from "react";
+// import Radium from "radium";
+import styles from './Car.module.css';
+
+class Car extends React.Component {
+    componentWillReceiveProps(nextProps, nextContext) {
+        console.log('Car componentWillReceiveProps', nextProps, nextContext)
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log('Car shouldComponentUpdate', nextProps, nextState, nextContext)
+        return nextProps.name.trim() === this.props.name.trim()
+    }
+
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        console.log('Car componentWillUpdate', nextProps, nextState, nextContext)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('Car componentDidUpdate', prevProps, prevState, snapshot)
+    }
+
+    render() {
+        console.log('Car render')
+        const inputClasses = [styles.input];
+
+        if (this.props.name !== ""){
+            inputClasses.push(styles.green)
+        } else {
+            inputClasses.push(styles.red)
+        }
+
+        if (this.props.name.length > 4){
+            inputClasses.push(styles.bold)
+        }
+
+        const style = {
+            border: '1px solid #ccc',
+            boxShadow: '0 4px 5px 0 rgba(0, 0, 0, 0.14)',
+            ':hover': {
+                border: '1px solid #aaa',
+                boxShadow: '0 4px 15px 0 rgba(0, 0, 0, 0.25)',
+                cursor: 'pointer'
+            }
+        }
+
+        return (
+            <div className={styles.Car} style={style}>
+                <h3>Car name: {this.props.name}</h3>
+                <p>Year: <strong>{this.props.year}</strong></p>
+                <input
+                    type="text"
+                    onChange={this.props.onChangeName}
+                    value={this.props.name}
+                    className={inputClasses.join(' ')}
+                />
+                <button onClick={this.props.onDelete}>Delete</button>
+            </div>
+        )
+    }
+}
+
+export default Car
