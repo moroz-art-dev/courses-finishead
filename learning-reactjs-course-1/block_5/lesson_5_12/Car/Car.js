bd1f9fdc5051714b819ea48b93b05a937e365aa8
moroz-art-dev <moroz.art.dev@gmail.com>
2021-08-20 02:44:23 +0300

    update of lessons in blocks 5

---
+++ learning-reactjs-course-1/block_5/lesson_5_12/Car/Car.js
@@ -0,0 +1,37 @@
+import React from "react";
+import classes from "./Car.module.css";
+import withClass from '../hoc/withClass'
+
+class Car extends React.Component {
+
+    render() {
+
+        const inputClasses = [classes.input];
+
+        if (this.props.name !== ""){
+            inputClasses.push(classes.green)
+        } else {
+            inputClasses.push(classes.red)
+        }
+
+        if (this.props.name.length > 4){
+            inputClasses.push(classes.bold)
+        }
+
+        return (
+            <React.Fragment>
+                <h3>Car name: {this.props.name}</h3>
+                <p>Year: <strong>{this.props.year}</strong></p>
+                <input
+                    type="text"
+                    onChange={this.props.onChangeName}
+                    value={this.props.name}
+                    className={inputClasses.join(' ')}
+                />
+                <button onClick={this.props.onDelete}>Delete</button>
+            </React.Fragment>
+        )
+    }
+}
+
+export default withClass(Car, classes.Car)
