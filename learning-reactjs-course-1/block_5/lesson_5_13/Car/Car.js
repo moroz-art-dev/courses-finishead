d3a3bccc7db0bcdf18316598526b9c5ce281b568
moroz-art-dev <moroz.art.dev@gmail.com>
2021-08-21 11:34:38 +0300

    end of lessons in blocks 5

---
+++ learning-reactjs-course-1/block_5/lesson_5_13/Car/Car.js
@@ -0,0 +1,45 @@
+import React from "react";
+import classes from "./Car.module.css";
+import PropTypes from 'prop-types'
+import withClass from '../hoc/withClass'
+
+class Car extends React.Component {
+
+    render() {
+
+        const inputClasses = [classes.input];
+
+        if (this.props.name !== ""){
+            inputClasses.push(classes.green)
+        } else {
+            inputClasses.push(classes.red)
+        }
+
+        if (this.props.name.length > 4){
+            inputClasses.push(classes.bold)
+        }
+
+        return (
+            <React.Fragment>
+                <h3>Car name: {this.props.name}</h3>
+                <p>Year: <strong>{this.props.year}</strong></p>
+                <input
+                    type="text"
+                    onChange={this.props.onChangeName}
+                    value={this.props.name}
+                    className={inputClasses.join(' ')}
+                />
+                <button onClick={this.props.onDelete}>Delete</button>
+            </React.Fragment>
+        )
+    }
+}
+
+Car.propTypes = {
+    name: PropTypes.string.isRequired,
+    year: PropTypes.number,
+    onChangeName: PropTypes.func,
+    onDelete: PropTypes.func
+}
+
+export default withClass(Car, classes.Car)
---
+++ learning-reactjs-course-1/block_5/lesson_5_14/App.js
@@ -0,0 +1,99 @@
+import {Component} from "react";
+import './App.scss';
+import Car from './Car/Car'
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
+import Counter from "./Counter/Counter";
+
+
+class App extends Component {
+
+    constructor(props) {
+        console.log('App constructor')
+        super(props);
+        this.state = {
+            cars: [
+                {'name': "Ford", 'year': 2018},
+                {'name': "Audi", 'year': 2016},
+                {'name': "Mazda", 'year': 2012},
+            ],
+            pageTitle: 'React components',
+            showCars: false
+        }
+    }
+
+    onChangeName(name, index) {
+        const car = this.state.cars[index]
+        car.name = name
+        const cars = [...this.state.cars]
+        cars[index] = car
+        this.setState({cars})
+    }
+
+    toggleCarsHandler = () => {
+        this.setState({
+            showCars: !this.state.showCars
+        })
+    }
+
+    deleteHandler(index) {
+        const cars = this.state.cars.concat()
+        cars.splice(index, 1)
+        this.setState({cars})
+    }
+
+    // componentWillMount() {
+    //     console.log('App componentWillMount')
+    // }
+    //
+    // componentDidMount() {
+    //     console.log('App componentDidMount')
+    // }
+
+    render() {
+       console.log('App render')
+
+       const divStyle = {
+           'textAlign': 'center'
+       }
+
+       let cars = null
+       if(this.state.showCars){
+           cars = this.state.cars.map((car, index)=>{
+               return (
+                   <ErrorBoundary  key={index}>
+                       <Car
+                           name={car.name}
+                           year={car.year}
+                           index={index}
+                           onDelete={this.deleteHandler.bind(this, index)}
+                           onChangeName={(event) => this.onChangeName(event.target.value, index)}
+                       />
+                   </ErrorBoundary>
+                  )
+           })
+       }
+
+       return (
+           <div style={divStyle}>
+               <h1>{this.props.title}</h1>
+
+               <Counter/>
+                <hr/>
+               <button
+                   style={{marginTop: '20px'}}
+                   className={'AppButton'}
+                   onClick={this.toggleCarsHandler}
+               >Toggle cars</button>
+                <div style={{
+                    width: 400,
+                    margin: 'auto',
+                    paddingTop: '20px'
+                }}>
+                    {cars}
+                </div>
+           </div>
+       );
+   }
+}
+
+export default App;
