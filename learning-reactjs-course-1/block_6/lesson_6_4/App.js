f12a60f808160f1de7b37edaa05f131afa50c67d
moroz-art-dev <moroz.art.dev@gmail.com>
2021-08-25 22:58:49 +0300

    add lessons in blocks 6

---
+++ learning-reactjs-course-1/block_6/lesson_6_4/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Layout from "./hoc/Layout/Layout"
+import Quiz from "./containers/Quiz/Quiz"
+
+function App() {
+  return (
+    <Layout>
+        <Quiz></Quiz>
+    </Layout>
+  )
+}
+
+export default App;
