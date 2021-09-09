c863560611c186f323c4e6a19dc22a95fe575a51
moroz-art-dev <moroz.art.dev@gmail.com>
2021-09-09 22:06:38 +0300

    add lessons in blocks 7

---
+++ learning-reactjs-course-1/block_7/lesson_7_3/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+import './App.scss'
+import {Route} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
+
+class App extends Component {
+  render() {
+
+    return (
+        <div>
+          <nav className="nav">
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/cars">Cars</a>
+              </li>
+            </ul>
+          </nav>
+
+          <hr/>
+          {/*localhost:3000*/}
+          <Route path="/" exact render={()=><h1>Home Page</h1>} />
+          <Route path="/about" component={About} />
+          <Route path="/cars" component={Cars} />
+
+        </div>
+    );
+  }
+}
+
+export default App
