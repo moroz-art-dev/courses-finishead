942a19bad68bf572312b8743f85a5dd23478f4f3
moroz-art-dev <moroz.art.dev@gmail.com>
2021-09-11 16:58:28 +0300

    end lessons in blocks 7

---
+++ learning-reactjs-course-1/block_7/lesson_7_9/App.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react'
+import './App.scss'
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
+import CarDetail from './CarDetail/CarDetail'
+
+class App extends Component {
+  render() {
+
+    return (
+        <div>
+          <nav className="nav">
+            <ul>
+              <li>
+                <NavLink
+                    to="/"
+                    exact
+                    activeClassName={'wfm-active'}
+                    >Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" activeStyle={{
+                    color: 'blue'
+                }}>About</NavLink>
+              </li>
+              <li>
+                <NavLink to={{
+                    pathname: '/cars'
+                }}>Cars</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <hr/>
+          {/*localhost:3000*/}
+            <Switch>
+                <Route path="/" exact render={()=><h1>Home Page</h1>} />
+                <Route path="/about" component={About} />
+                <Route path="/cars/:name" component={CarDetail} />
+                <Route path="/cars" component={Cars} />
+                <Redirect to={'/'}/>
+                {/*<Route render={()=><h1 style={{color: 'red', textAlign: 'center'}}>404 not found</h1>}/>*/}
+            </Switch>
+        </div>
+    );
+  }
+}
+
+export default App
